2437c6bd9a2ee6b51af3fd406d77068a77a20752
joaoagripino <maia.joaoagripino@gmail.com>
2023-04-23 19:27:22 -0300

    Completed Prep 2 code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string percentGrade = Console.ReadLine();
+        int number = int.Parse(percentGrade);
+        
+        string lgradeLetter = "";
+
+        if (percent >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (percent >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (percent >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (percent >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+    
 }
